e2e6e6c917ebbfb64279708a0eb813e66404a498
Juan Martin Marco <jmmarco@gmail.com>
2017-10-18 19:42:16 -0300

    Add initial helper file

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+/*
+To manage your AsyncStorage database, you'll want to create four different helper methods.
+
+getDecks: return all of the decks along with their titles, questions, and answers.
+getDeck: take in a single id argument and return the deck associated with that id.
+saveDeckTitle: take in a single title argument and add it to the decks.
+addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+
+*/
