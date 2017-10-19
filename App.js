2d6081c172603b142f477de20f1c587724201bcf
Juan Martin Marco <jmmarco@gmail.com>
2017-10-19 10:50:36 -0300

    Add react navigation and tabs

--- App.js
+++ App.js
@@ -1,26 +1,64 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native'
+import { MaterialCommunityIcons, MaterialIcons, Ionicons, FontAwesome } from '@expo/vector-icons'
+import NewDeckView from './components/NewDeckView'
+import DeckListView from './components/DeckListView'
+import { TabNavigator } from 'react-navigation'
+import { mediumSeaGreen, white } from './utils/colors'
+import { Constants } from 'expo'
+
+function FlashCardStatusBar ({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: DeckListView,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor} />
+    },
+  },
+  NewDeckView: {
+    screen: NewDeckView,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <MaterialIcons name='add' size={30} color={tintColor} />
+    },
+  },
+
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? mediumSeaGreen : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : mediumSeaGreen,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.mainTitle}>Mobile Flashcards</Text>
-        <MaterialCommunityIcons name='cards-outline' style={{color: 'deepskyblue'}} size={50}/>
+      <View style={{flex: 1}}>
+        <FlashCardStatusBar backgroundColor={mediumSeaGreen} barStyle='light-content'/>
+        <Tabs/>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mainTitle: {
-    fontSize: 30,
-  }
-})
