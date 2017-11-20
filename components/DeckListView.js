ff46cf49dbd0236776638df8f886e7e337583dac
Juan Martin Marco <jmmarco@gmail.com>
2017-11-20 12:20:57 -0300

    Display each deck

--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { FLASHCARDS_STORAGE_KEY, initialFlashCards} from '../utils/_initialData'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { getDecks } from '../utils/api'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { AsyncStorage } from 'react-native'
 
 export default class DeckListView extends Component {
@@ -8,14 +9,17 @@ export default class DeckListView extends Component {
   constructor() {
     super()
     this.state = {
-      firstLaunch: null
+      firstLaunch: null,
+      decks: []
     }
   }
 
   componentDidMount() {
     AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(initialFlashCards), () => {
-      AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY, (err, result) => {
-        console.log(JSON.parse(result))
+      AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY, (err, results) => {
+        this.setState({
+          decks: JSON.parse(results)
+        })
       })
     })
     .then((response) => {
@@ -26,17 +30,36 @@ export default class DeckListView extends Component {
       // Otherwise output error to the console
       console.log("Oops, something went wrong..", error)
     })
+
   }
 
 
+
   render() {
-    if (this.state.firstLaunch) {
+
+    const decks = Object.keys(this.state.decks)
+
+    decks.map((entry) => {
+      console.log(entry)
+    })
+
+    if (this.state.firstLaunch && decks.length > 0) {
       return (
+
         <View>
-          <Text>{JSON.stringify(this.state.firstLaunch)}</Text>
-          <Text>This is the DeckListView with AsyncStorage initialized!</Text>
+          {decks.map((entry) => {
+            return (
+              <TouchableOpacity key={entry} style={styles.deck}>
+                <Text style={styles.deckTitle}>{entry}</Text>
+                <Text>{decks.length} cards</Text>
+              </TouchableOpacity>
+            )
+
+          })}
+
         </View>
-      )
+          )
+
     } else {
       return (
         <View>
@@ -46,9 +69,23 @@ export default class DeckListView extends Component {
       )
     }
 
-    // return (
-    //   <Text>This is the DeckListView Component</Text>
-    // )
+
   }
 
 }
+
+const styles = StyleSheet.create({
+  deck: {
+    padding: 10,
+    margin: 10,
+    borderWidth: 2,
+    borderColor: '#d6d7da',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  deckTitle: {
+    fontSize: 18,
+    letterSpacing: 1,
+  }
+
+})
