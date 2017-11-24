4fb0dd2b3291daa769b87d024ce5ae5c3a30c0d5
Juan Martin Marco <jmmarco@gmail.com>
2017-11-24 10:09:07 -0300

    Add a bit of detail

--- components/IndividualDeckView.js
+++ components/IndividualDeckView.js
@@ -3,9 +3,20 @@ import { View, TouchableOpacity, Text } from 'react-native'
 
 export default class IndividualDeckView extends Component {
 
+  static navigationOptions = ({ navigation }) => {
+    const { deckId, title } = navigation.state.params
+
+    return {
+      title: title
+    }
+  }
   render() {
+    const { deckId } = this.props.navigation.state.params
+    console.log(deckId)
     return (
-      <Text>This is the IndividualDeckView Component</Text>
+      <View>
+        <Text>This is the IndividualDeckView Component - Deck id:{deckId}</Text>
+      </View>
     )
   }
 
