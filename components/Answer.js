33e62c3d9879181048c4f3b7fb1bbed7dd473e58
Juan Martin Marco <jmmarco@gmail.com>
2017-12-04 13:21:58 -0300

    Refactor into a Stateless Component

--- components/Answer.js
+++ components/Answer.js
@@ -3,28 +3,24 @@ import { View, TouchableOpacity, Text } from 'react-native'
 import styles from '../utils/styles'
 import { MaterialCommunityIcons, MaterialIcons, Ionicons, FontAwesome } from '@expo/vector-icons'
 
-export default class Answer extends Component {
 
-  static navigationOptions = ({ navigation }) => {
 
-    return {
-      title: 'Answer',
-    }
 
-  }
+const Answer = (props) => {
+  const answer = props.navigation.state.params
+  return (
+    <View style={styles.container}>
+      <Ionicons name='md-text' size={100} color={'black'}/>
+      <Text style={styles.answerText}>{answer}</Text>
+    </View>
 
-  render() {
-
-    const answer = this.props.navigation.state.params
-
-    return (
-      <View style={styles.container}>
-
-        <Ionicons name='md-text' size={100} color={'black'}/>
-        <Text style={styles.answerText}>{answer}</Text>
-      </View>
-
-    )
-  }
-
+  )
 }
+
+Answer.navigationOptions = ({ navigation }) => {
+  return {
+    title: 'Answer',
+  }
+}
+
+export default Answer
